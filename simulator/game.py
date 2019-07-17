e673c5a1e8abf2e983e005bb02892f82e3c890d6
Monte Hellawell <monte@montudor.com>
2019-07-17 23:00:46 +0100

    Add Person object

--- simulator/game.py
+++ simulator/game.py
@@ -1,8 +1,56 @@
 from . import errors
 import random
 
-class Actor(object):
+class Person(object):
+    GENDER_MALE = 1
+    GENDER_FEMALE = 2
+    GENDER_RANDOM = 3
+    GENDER_OTHER = 4
+
+    def __init__(self, *args, **kwargs):
+        self.first_name = kwargs.get("first_name", None)
+        self.last_name = kwargs.get("last_name", None)
+        self.set_gender(kwargs.get("gender", self.GENDER_RANDOM))
+
+    def set_gender(self, gender):
+        if gender in [self.GENDER_MALE, self.GENDER_FEMALE, self.GENDER_OTHER]:
+            self.gender = gender
+        elif gender == self.GENDER_RANDOM:
+            self.gender = random.choice([self.GENDER_MALE, self.GENDER_FEMALE])
+        else:
+            self.gender = self.GENDER_OTHER
+
+    def get_gender_text(self):
+        if self.gender == self.GENDER_MALE:
+            return "male"
+        elif self.gender == self.GENDER_FEMALE:
+            return "female"
+        else:
+            return "other"
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def name(self):
+        return "%s %s" % (self.first_name, self.last_name)
+
+
+class Actor(Person):
     events = []
+    people = {
+        "family": {
+            "mother": None,
+            "father": None
+        },
+        "friends": {
+            "best_friend": None
+        },
+        "relationship": {
+            "person": None,
+            "type": None
+        }
+    }
     ticks = 0
     _loaded = False
 
@@ -11,6 +59,7 @@ class Actor(object):
         self.config = game.get_config()
         self.first_name = kwargs.get("first_name", None)
         self.last_name = kwargs.get("last_name", None)
+        self.gender = kwargs.get("gender", None)
         self.primary = kwargs.get("primary", False)
         self.tier = kwargs.get("tier", None)
         self.age = kwargs.get("age", 0)
@@ -62,10 +111,6 @@ class Actor(object):
         return self.trigger_random_event()
 
 
-class Person(object):
-    pass
-
-
 class Event(object):
     def __init__(self, game, *args, **kwargs):
         self.game = game
@@ -74,13 +119,14 @@ class Event(object):
     
     def get_event_text(self, *args, **kwargs):
         return self.text.format(
-            actor=self.game.current_actor
+            actor=self.game.current_actor,
+            gender=self.game.current_actor.get_gender_text().capitalize()
         )
 
 
 class Game(object):
     config = None
-    _name_pool = None
+    _people_pool = None
     active = False
 
     def __init__(self, *args, **kwargs):
@@ -94,22 +140,43 @@ class Game(object):
             self.config = json.loads(file.read())
         return self.config
 
-    def get_name_pool(self, force_gen=False):
-        if self._name_pool is not None and force_gen is not True:
-            return self._name_pool
+    def get_people(self, force_gen=False):
+        if self._people_pool is not None and force_gen is not True:
+            return self._people_pool
         config = self.get_config()
-        names = []
+        people = []
 
         first_names = []
         for key, items in config["names"]["first_names"].items():
             for name in config["names"]["first_names"][key]:
-                first_names.append(name)
+                genders = []
+                duplicate = False
+                if key == "male":
+                    genders = [Person.GENDER_MALE]
+                elif key == "female":
+                    genders = [Person.GENDER_FEMALE]
+                elif key == "gender_neutral":
+                    genders = [Person.GENDER_MALE, Person.GENDER_FEMALE, Person.GENDER_OTHER]
+                else:
+                    gender = Person.GENDER_OTHER
+                for gender in genders:
+                    first_names.append(
+                        {
+                            "first_name": name,
+                            "gender": gender
+                        }
+                    )
 
-        for fn in first_names:
+        for person in first_names:
             for ln in config["names"]["last_names"]:
-                names.append("%s %s" % (fn, ln))
-        self._name_pool = names
-        return names
+                new = Person(
+                    first_name=person["first_name"],
+                    last_name=ln,
+                    gender=person["gender"]
+                )
+                people.append(new)
+        self._people_pool = people
+        return people
 
     def get_starter_tier(self):
         for tier in self.config.get("events", {}).get("meta", {}).get("tiers", [None]):
@@ -123,22 +190,22 @@ class Game(object):
             raise errors.InvalidConfig()
 
     def generate_actor(self):
-        name_pool = self.get_name_pool()
-        pos = random.randint(0, len(name_pool)-1)
-        name = name_pool[pos]
-        del name_pool[pos]
-        actor_name = name.split(" ")
+        pool = self.get_people()
+        pos = random.randint(0, len(pool)-1)
+        person = pool[pos]
+        del pool[pos]
         actor = Actor(
             self,
-            first_name=actor_name[0],
-            last_name=actor_name[1],
+            first_name=person.first_name,
+            last_name=person.last_name,
+            gender=person.gender,
             primary=True,
             tier=self.get_starter_tier()
         )
         return actor
 
     def new(self):
-        self._name_pool = self.get_name_pool(force_gen=True)
+        self.get_people(force_gen=True)
         self.current_actor = self.generate_actor()
         self.active = True
 
